55c3329ccf0f040be798a9b7a74fed4d3a1961b5
rahimtulla4 <rahimtulla4@gmail.com>
2018-01-26 17:12:48 +0530

    initial upload

---
+++ src/com/example/alarm/MyBroadcastReceiver.java
@@ -0,0 +1,24 @@
+package com.example.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+
+/**
+ * @author Prabu
+ *
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+ @Override
+ public void onReceive(Context context, Intent intent) {
+    Toast.makeText(context, "Time is up!!!!.",
+   Toast.LENGTH_LONG).show();
+    // Vibrate the mobile phone
+    Vibrator vibrator = (Vibrator) context
+  .getSystemService(Context.VIBRATOR_SERVICE);
+    vibrator.vibrate(2000);
+ }
+}
